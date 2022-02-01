0b1a6c6bd3600faad1864ac456719cbc47a8b9d6
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-02-01 15:23:08 +0530

    added email class

---
+++ Email.java
@@ -0,0 +1,13 @@
+class Email{
+    private String emailId;
+
+    public Email(String emailId) {
+        this.emailId = emailId;
+    }
+    public String getEmailId(){
+        return emailId;
+    }
+    public void setEmailid(String newEmailId){
+        this.emailId = newEmailId;
+    }
+}
