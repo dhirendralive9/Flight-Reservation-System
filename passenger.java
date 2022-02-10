025b1faaee909871b93e32f9974dfcf681e803d0
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-02-10 10:07:41 +0530

    checkpoint 3

--- passenger.java
+++ passenger.java
@@ -1,43 +1,49 @@
 class Passenger{
-    private int passengerId; //private id for storing unique id for passenger.
-    private Address address; //private object for address class 
-    private Contact contact; //private object for contact class
+    private static int idCounter;  //private id for storing unique id for passenger.
+    private static Address address; //private object for address class 
+    private static Contact contact; //private object for contact class
+    private static int passengerCount =0; //This will count passenger objectcreated
 
     public Passenger(String name,String emailId,long phoneNumber,String addres){
-       contact = new Contact(name, emailId, phoneNumber);
-       address = new Address(addres);
+       Passenger.contact = new Contact(name, emailId, phoneNumber);
+       Passenger.address =new Address(addres);
+       Passenger.idCounter = (int)(Math.random() * 999999);
     }  //here we use constructor, to assign values in the passenger and contact object.
 
 
+    public int getpassengerCount(){
+      return Passenger.passengerCount;
+    }
+
     public int getPassengerId(){  //This will generate userid for passenger.
-        passengerId = (int)(Math.random() * 999999);
-        return passengerId;
+        
+        return Passenger.idCounter;
     }
 
      public void setContactName(String name){   //Setters for Phone no, email id, phone number and address 
-       this.contact.setName(name);
+       Passenger.contact.setName(name);
      }
      public void setContactemailId(String emailId){
-         this.contact.setEmailId(emailId);
+         Passenger.contact.setEmailId(emailId);
     }
     public void setContactPhoneNumber(long phoneNumber){
-         this.contact.setPhoneNumber(phoneNumber);
+         Passenger.contact.setPhoneNumber(phoneNumber);
     }
     public void setPassengerAddress(String address){
-         this.address.setAddress(address);
+         Passenger.address.setAddress(address);
     }
 
     public String getContactName(){       //getters for contact,email id, phonenumber and address.
-        return this.contact.getName();
+        return Passenger.contact.getName();
     }
     public String getContactEmailId(){
-        return this.contact.getEmailId();
+        return Passenger.contact.getEmailId();
     }
     public long getContactPhoneNumber(){
-        return this.contact.getPhoneNumber();
+        return Passenger.contact.getPhoneNumber();
     }
     public String getPassengerAddress(){
-        return this.address.getAddress();
+        return Passenger.address.getAddress();
     }
     
 }
